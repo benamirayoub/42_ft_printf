7e4c3ef4b762d3760659690cbb6eadaead43836d
benamirayoub <benamarayoub29@gmail.com>
2026-09-26 18:26:28 +0200

    libft

---
+++ libft/libft.h
@@ -0,0 +1,11 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isprint(int c);
+int		ft_atoi(const char *str);
